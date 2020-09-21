a55c12d6eb3b5df71aac64116580337b9396f5b7
azanirr <azanirr212@gmail.com>
2020-09-21 13:07:00 +0000

    Updates

---
+++ precache-manifest.7b68cc29ff5e6abb04af393e6aaa08c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92030d610b8b2ea91eac190806e4638",
+    "url": "/todoListWithReactJS/index.html"
+  },
+  {
+    "revision": "450e3c5372c1e318219c",
+    "url": "/todoListWithReactJS/static/css/main.cdfada3b.chunk.css"
+  },
+  {
+    "revision": "d2ce6b877e02fc9f2dc6",
+    "url": "/todoListWithReactJS/static/js/2.c2b0b9c7.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/todoListWithReactJS/static/js/2.c2b0b9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450e3c5372c1e318219c",
+    "url": "/todoListWithReactJS/static/js/main.3ac3d086.chunk.js"
+  },
+  {
+    "revision": "9c248d0229cf22400abe",
+    "url": "/todoListWithReactJS/static/js/runtime-main.02037c7d.js"
+  }
+]);
